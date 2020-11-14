d6180aa538c2709ca8cfa9bc562984d12e23895b
andres.andrade <andres.andrade@electronicid.eu>
2020-11-14 18:09:33 +0100

    4

--- src/main/es/brujula/Main.java
+++ src/main/es/brujula/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
     private Main(){
 
-        //Nothing to do here..
+        //Nothing to do here....
 
     }
 
     public static void main(String[] args) throws BrujulaException{
 
-       //Nothing to do here..
+       //Nothing to do here....
 
     }
 }
