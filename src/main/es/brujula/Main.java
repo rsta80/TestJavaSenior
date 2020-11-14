75fb778bfb01e16d43aa7ffda988af632d8655b6
andres.andrade <andres.andrade@electronicid.eu>
2020-11-14 17:59:14 +0100

    Bump junit from 4.12 to 4.13.1 in /bin

--- src/main/es/brujula/Main.java
+++ src/main/es/brujula/Main.java
@@ -5,7 +5,9 @@ import es.brujula.exception.BrujulaException;
 public class Main {
 
     private Main(){
+
         //Nothing to do here...
+
     }
 
     public static void main(String[] args) throws BrujulaException{
